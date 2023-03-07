5122e06db85ddbfb2dc93c5290481f9420d937bd
NamHoang07 <124911859+NamHoang07@users.noreply.github.com>
2023-03-07 21:01:16 +0700

    card

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Quỳnh Hương';
+const giftUrl = 'https://www.facebook.com/pit.pepa';
+const eventName = 'Chúc Mừng 8-3';
+const titleCard = 'Tặng QH';
+const contentCard = 'Chúc Hương 8-3 hạnh phúc, xinh đẹp, học giỏi hơn nha! Biết Hương khá là mệt ó nên Nam mong là cái thiệp này sẽ mang đến 1 năng lượng tích cực nhá :D';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'lqh1.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
